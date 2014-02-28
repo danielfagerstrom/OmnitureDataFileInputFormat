f345ca1060403939c16f56b3c43a476ee6fcce43
Daniel Fagerström <daniel.fagerstrom@centralen.schibsted.se>
2014-02-28 16:45:52 +0100

    The latest version of the Moniture data uses 554 fields.

---
+++ src/com/tgam/hadoop/mapreduce/OmnitureDataFileInputFormat554.java
@@ -0,0 +1,18 @@
+package com.tgam.hadoop.mapreduce;
+
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+/**
+ * A custom input format for dealing with Omniture's hit_data.tsv daily data feed files.
+ * @author Mike Sukmanowsky (<a href="mailto:mike.sukmanowsky@gmail.com">mike.sukmanowsky@gmail.com</a>)
+ *
+ */
+public class OmnitureDataFileInputFormat554 extends OmnitureDataFileInputFormat {
+	
+	@Override
+	public OmnitureDataFileRecordReader554 createRecordReader(InputSplit split, TaskAttemptContext tac) {
+		return new OmnitureDataFileRecordReader554();
+	}
+
+}
